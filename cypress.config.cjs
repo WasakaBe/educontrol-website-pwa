9b1ccccf45df206de83645eaec64183d18a7db23
WasakaBe <aureliabelindaalandejesus@gmail.com>
2024-10-22 17:28:03 -0600

    agg cypress.config.cjs

---
+++ cypress.config.cjs
@@ -0,0 +1,12 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Aquí puedes agregar eventos personalizados si es necesario
+    },
+    baseUrl: 'http://localhost:4173', // Ajusta el puerto si es necesario
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Define el patrón para encontrar tus pruebas
+    supportFile: false, // Desactiva el archivo de soporte si no necesitas configuraciones avanzadas
+  },
+})

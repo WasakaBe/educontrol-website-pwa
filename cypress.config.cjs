43ec1b103039e35d122be398a7746ce6deefabe9
WasakaBe <aureliabelindaalandejesus@gmail.com>
2024-10-22 21:33:36 -0600

    mejoramiento del paso 8 v4

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -6,7 +6,6 @@ module.exports = defineConfig({
       // Aquí puedes agregar eventos personalizados si es necesario
     },
     baseUrl: 'http://localhost:5173', // Ajusta el puerto si es necesario
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Define el patrón para encontrar tus pruebas
     supportFile: false, // Desactiva el archivo de soporte si no necesitas configuraciones avanzadas
   },
 })

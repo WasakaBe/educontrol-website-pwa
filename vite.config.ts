57f59a218cf88a722f78f6419d853b9868af1209
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-10-24 22:14:27 -0600

    Revert "Cambios en configuracion vite"

--- vite.config.ts
+++ vite.config.ts
@@ -56,13 +56,12 @@ export default defineConfig({
         theme_color: '#000',
         background_color: '#074d17',
         display: 'standalone',
-        icons: [// Lista de iconos para la aplicación
+        icons: [
           {
-            src: './logo_cbta_5.png', // Ruta al icono
-            sizes: '977x1279', // Tamaño del icono
-            type: 'image/png', // Tipo de imagen
+            src: './logo_cbta_5.png',
+            sizes: '977x1279',
+            type: 'image/png',
           },
-
           {
             src: './logocbtapequeno.png',
             sizes: '144x144',

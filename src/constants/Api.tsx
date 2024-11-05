9d0a8b3f6ed5cbd428e067e9c135d62c50b56e9e
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-11-04 19:59:39 -0600

    Update Api.tsx

--- src/constants/Api.tsx
+++ src/constants/Api.tsx
@@ -1,1 +1,2 @@
-export const apiUrl: string = "http://localhost:50023/api/";
+//export const apiUrl: string = "http://localhost:50023/api/";
+export const apiUrl: string = "https://c2e0-129-222-90-104.ngrok-free.app/api/";

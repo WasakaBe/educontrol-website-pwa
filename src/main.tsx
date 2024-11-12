ff9132f4ee73d450f98fd0c3bd2fd2eac3fd5978
WasakaBe <aureliabelindaalandejesus@gmail.com>
2024-11-12 11:04:06 -0600

    DESPLIEGUE CONTINUO DE SOFTWARE, MONITOREO DE SOFTWARE Y PROCESO DE EVALUACIÓN DE RENDIMIENTO v5

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,9 @@ Sentry.init({
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  release: "eb4d6b0405eedf5c25f58e6d4b0933185fb2d3bb@0.1.0",
 });
+
 
 import React, { useState, useEffect, useContext } from 'react'; 
 import ReactDOM from 'react-dom/client';
